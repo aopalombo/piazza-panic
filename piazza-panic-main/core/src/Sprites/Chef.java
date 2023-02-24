a86c8e0bfbd0715cb3901ba2c117b220d14410e2
Immanuel Ghaly <great.imo@gmail.com>
2023-02-24 11:32:09 +0000

    Renaming a few files

--- piazza-panic-main/core/src/Sprites/Chef.java
+++ piazza-panic-main/core/src/Sprites/Chef.java
@@ -5,7 +5,6 @@ import Recipe.BurgerRecipe;
 import Recipe.Recipe;
 import Recipe.SaladRecipe;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
